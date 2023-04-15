5e91a31e285d65d05e3207ad42961c56799b47e8
gong <genzhedangdang@gmail.com>
2023-04-15 16:42:02 +0800

    fix codestyle

--- inlong-sort/sort-connectors/mysql-cdc/src/main/java/org/apache/inlong/sort/cdc/mysql/debezium/task/MySqlBinlogSplitReadTask.java
+++ inlong-sort/sort-connectors/mysql-cdc/src/main/java/org/apache/inlong/sort/cdc/mysql/debezium/task/MySqlBinlogSplitReadTask.java
@@ -103,20 +103,20 @@ public class MySqlBinlogSplitReadTask extends MySqlStreamingChangeEventSource {
         final BinaryLogClient client = taskContext.getBinaryLogClient();
         final List<EventListener> eventListeners = client.getEventListeners();
         final List<BinaryLogClient.LifecycleListener> lifecycleListeners =
-            client.getLifecycleListeners();
+                client.getLifecycleListeners();
 
         eventListeners.forEach(
-            listener -> {
-                if (eventListeners.indexOf(listener) != eventListeners.size() - 1) {
-                    client.unregisterEventListener(listener);
-                }
-            });
+                listener -> {
+                    if (eventListeners.indexOf(listener) != eventListeners.size() - 1) {
+                        client.unregisterEventListener(listener);
+                    }
+                });
         lifecycleListeners.forEach(
-            listener -> {
-                if (lifecycleListeners.indexOf(listener) != lifecycleListeners.size() - 1) {
-                    client.unregisterLifecycleListener(listener);
-                }
-            });
+                listener -> {
+                    if (lifecycleListeners.indexOf(listener) != lifecycleListeners.size() - 1) {
+                        client.unregisterLifecycleListener(listener);
+                    }
+                });
 
         super.execute(context);
     }
