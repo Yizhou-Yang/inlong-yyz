7948f3cfceeca371623c8a5d6c6e3a72120e6dd9
ganfengtan <ganfengtan@tencent.com>
2023-04-17 12:11:02 +0800

    解决iceberg dlc 提交缺少summary等信息

--- inlong-sort/sort-connectors/iceberg-dlc/src/main/java/org/apache/inlong/sort/iceberg/catalog/hybris/HiveTableOperations.java
+++ inlong-sort/sort-connectors/iceberg-dlc/src/main/java/org/apache/inlong/sort/iceberg/catalog/hybris/HiveTableOperations.java
@@ -47,8 +47,11 @@ import org.apache.hadoop.hive.metastore.api.Table;
 import org.apache.hadoop.hive.metastore.api.hive_metastoreConstants;
 import org.apache.iceberg.BaseMetastoreTableOperations;
 import org.apache.iceberg.ClientPool;
+import org.apache.iceberg.PartitionSpecParser;
+import org.apache.iceberg.SchemaParser;
 import org.apache.iceberg.Snapshot;
 import org.apache.iceberg.SnapshotSummary;
+import org.apache.iceberg.SortOrderParser;
 import org.apache.iceberg.TableMetadata;
 import org.apache.iceberg.TableProperties;
 import org.apache.iceberg.common.DynMethods;
@@ -66,8 +69,10 @@ import org.apache.iceberg.relocated.com.google.common.collect.ImmutableBiMap;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
 import org.apache.iceberg.relocated.com.google.common.collect.Lists;
 import org.apache.iceberg.relocated.com.google.common.collect.Maps;
+import org.apache.iceberg.shaded.com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.iceberg.shaded.com.github.benmanes.caffeine.cache.Cache;
 import org.apache.iceberg.shaded.com.github.benmanes.caffeine.cache.Caffeine;
+import org.apache.iceberg.util.JsonUtil;
 import org.apache.iceberg.util.Tasks;
 import org.apache.thrift.TException;
 import org.slf4j.Logger;
@@ -103,8 +108,15 @@ public class HiveTableOperations extends BaseMetastoreTableOperations {
             .build();
     private static final BiMap<String, String> ICEBERG_TO_HMS_TRANSLATION = ImmutableBiMap.of(
             // gc.enabled in Iceberg and external.table.purge in Hive
-            // are meant to do the same things but with different names
-            GC_ENABLED, "external.table.purge");
+            //      are meant to do the same things but with different names
+            GC_ENABLED, "external.table.purge"
+    );
+    // the max size is based on HMS backend database. For Hive versions below 2.3, the max table
+    // parameter size is 4000
+    // characters, see https://issues.apache.org/jira/browse/HIVE-12274
+    // set to 0 to not expose Iceberg metadata in HMS Table properties.
+    private static final String HIVE_TABLE_PROPERTY_MAX_SIZE = "iceberg.hive.table-property-max-size";
+    private static final long HIVE_TABLE_PROPERTY_MAX_SIZE_DEFAULT = 32672;
 
     private static Cache<String, ReentrantLock> commitLockCache;
 
@@ -150,6 +162,7 @@ public class HiveTableOperations extends BaseMetastoreTableOperations {
     private final long lockCheckMaxWaitTime;
     private final int metadataRefreshMaxRetries;
     private final FileIO fileIO;
+    private final long maxHiveTablePropertySize;
     private final ClientPool<IMetaStoreClient, TException> metaClients;
 
     protected HiveTableOperations(Configuration conf, ClientPool metaClients, FileIO fileIO,
@@ -171,6 +184,8 @@ public class HiveTableOperations extends BaseMetastoreTableOperations {
                         HIVE_ICEBERG_METADATA_REFRESH_MAX_RETRIES_DEFAULT);
         long tableLevelLockCacheEvictionTimeout =
                 conf.getLong(HIVE_TABLE_LEVEL_LOCK_EVICT_MS, HIVE_TABLE_LEVEL_LOCK_EVICT_MS_DEFAULT);
+        this.maxHiveTablePropertySize =
+                conf.getLong(HIVE_TABLE_PROPERTY_MAX_SIZE, HIVE_TABLE_PROPERTY_MAX_SIZE_DEFAULT);
         initTableLevelLockCache(tableLevelLockCacheEvictionTimeout);
     }
 
@@ -405,8 +420,92 @@ public class HiveTableOperations extends BaseMetastoreTableOperations {
         if (summary.get(SnapshotSummary.TOTAL_FILE_SIZE_PROP) != null) {
             parameters.put(StatsSetupConst.TOTAL_SIZE, summary.get(SnapshotSummary.TOTAL_FILE_SIZE_PROP));
         }
-
+        setSnapshotStats(metadata, parameters);
+        setSchema(metadata, parameters);
+        setPartitionSpec(metadata, parameters);
+        setSortOrder(metadata, parameters);
         tbl.setParameters(parameters);
+    }
+
+    @VisibleForTesting
+    void setSnapshotStats(TableMetadata metadata, Map<String, String> parameters) {
+        parameters.remove(TableProperties.CURRENT_SNAPSHOT_ID);
+        parameters.remove(TableProperties.CURRENT_SNAPSHOT_TIMESTAMP);
+        parameters.remove(TableProperties.CURRENT_SNAPSHOT_SUMMARY);
+
+        Snapshot currentSnapshot = metadata.currentSnapshot();
+        if (exposeInHmsProperties() && currentSnapshot != null) {
+            parameters.put(
+                    TableProperties.CURRENT_SNAPSHOT_ID, String.valueOf(currentSnapshot.snapshotId()));
+            parameters.put(
+                    TableProperties.CURRENT_SNAPSHOT_TIMESTAMP,
+                    String.valueOf(currentSnapshot.timestampMillis()));
+            setSnapshotSummary(parameters, currentSnapshot);
+        }
+
+        parameters.put(TableProperties.SNAPSHOT_COUNT, String.valueOf(metadata.snapshots().size()));
+    }
+
+    @VisibleForTesting
+    void setSnapshotSummary(Map<String, String> parameters, Snapshot currentSnapshot) {
+        try {
+            String summary = JsonUtil.mapper().writeValueAsString(currentSnapshot.summary());
+            if (summary.length() <= maxHiveTablePropertySize) {
+                parameters.put(TableProperties.CURRENT_SNAPSHOT_SUMMARY, summary);
+            } else {
+                LOG.warn(
+                        "Not exposing the current snapshot({}) summary in HMS since it exceeds {} characters",
+                        currentSnapshot.snapshotId(),
+                        maxHiveTablePropertySize);
+            }
+        } catch (JsonProcessingException e) {
+            LOG.warn(
+                    "Failed to convert current snapshot({}) summary to a json string",
+                    currentSnapshot.snapshotId(),
+                    e);
+        }
+    }
+
+    @VisibleForTesting
+    void setSchema(TableMetadata metadata, Map<String, String> parameters) {
+        parameters.remove(TableProperties.CURRENT_SCHEMA);
+        if (exposeInHmsProperties() && metadata.schema() != null) {
+            String schema = SchemaParser.toJson(metadata.schema());
+            setField(parameters, TableProperties.CURRENT_SCHEMA, schema);
+        }
+    }
+
+    @VisibleForTesting
+    void setPartitionSpec(TableMetadata metadata, Map<String, String> parameters) {
+        parameters.remove(TableProperties.DEFAULT_PARTITION_SPEC);
+        if (exposeInHmsProperties() && metadata.spec() != null && metadata.spec().isPartitioned()) {
+            String spec = PartitionSpecParser.toJson(metadata.spec());
+            setField(parameters, TableProperties.DEFAULT_PARTITION_SPEC, spec);
+        }
+    }
+
+    @VisibleForTesting
+    void setSortOrder(TableMetadata metadata, Map<String, String> parameters) {
+        parameters.remove(TableProperties.DEFAULT_SORT_ORDER);
+        if (exposeInHmsProperties()
+                && metadata.sortOrder() != null
+                && metadata.sortOrder().isSorted()) {
+            String sortOrder = SortOrderParser.toJson(metadata.sortOrder());
+            setField(parameters, TableProperties.DEFAULT_SORT_ORDER, sortOrder);
+        }
+    }
+
+    private void setField(Map<String, String> parameters, String key, String value) {
+        if (value.length() <= maxHiveTablePropertySize) {
+            parameters.put(key, value);
+        } else {
+            LOG.warn(
+                    "Not exposing {} in HMS since it exceeds {} characters", key, maxHiveTablePropertySize);
+        }
+    }
+
+    private boolean exposeInHmsProperties() {
+        return maxHiveTablePropertySize > 0;
     }
 
     private StorageDescriptor storageDescriptor(TableMetadata metadata, boolean hiveEngineEnabled) {
