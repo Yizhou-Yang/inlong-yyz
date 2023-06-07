4e10335bf0b5eeb11216051154f211698981ebcd
lordcheng10 <lordcheng1020@gmail.com>
2023-06-07 15:18:52 +0800

    --story=883914479 【技术优化】【DDL】技术方案优化统一 (merge request !493)
    Squash merge branch '2.8.3_ddl' into 'feature-2.8.3'
    Support data inference schema change type and iceberg ddl change

--- inlong-sort/sort-connectors/iceberg/src/main/java/org/apache/inlong/sort/iceberg/sink/FlinkSink.java
+++ inlong-sort/sort-connectors/iceberg/src/main/java/org/apache/inlong/sort/iceberg/sink/FlinkSink.java
@@ -175,6 +175,9 @@ public class FlinkSink {
         private @Nullable DirtySink<Object> dirtySink;
         private ReadableConfig tableOptions;
 
+        private boolean enableSchemaChange;
+        private String schemaChangePolicies;
+
         private Builder() {
         }
 
@@ -273,6 +276,16 @@ public class FlinkSink {
         public Builder overwrite(boolean newOverwrite) {
             this.overwrite = newOverwrite;
             writeOptions.put(FlinkWriteOptions.OVERWRITE_MODE.key(), Boolean.toString(newOverwrite));
+            return this;
+        }
+
+        public Builder enableSchemaChange(boolean schemaChange) {
+            this.enableSchemaChange = schemaChange;
+            return this;
+        }
+
+        public Builder schemaChangePolicies(String schemaChangePolicies) {
+            this.schemaChangePolicies = schemaChangePolicies;
             return this;
         }
 
@@ -616,7 +629,8 @@ public class FlinkSink {
 
             int parallelism = writeParallelism == null ? input.getParallelism() : writeParallelism;
             DynamicSchemaHandleOperator routeOperator = new DynamicSchemaHandleOperator(
-                    catalogLoader, multipleSinkOption, dirtyOptions, dirtySink, inlongMetric, auditHostAndPorts);
+                    catalogLoader, multipleSinkOption, dirtyOptions, dirtySink, inlongMetric, auditHostAndPorts,
+                    enableSchemaChange, schemaChangePolicies);
             SingleOutputStreamOperator<RecordWithSchema> routeStream = input
                     .transform(operatorName(ICEBERG_WHOLE_DATABASE_MIGRATION_NAME),
                             TypeInformation.of(RecordWithSchema.class),
